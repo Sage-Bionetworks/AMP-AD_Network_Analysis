f5540794c0b063461c414d13a6bc0c607b039c3e
Benjamin Logsdon <ben.logsdon@sagebase.org>
2016-12-29 12:56:30 -0800

    first pass of script

---
+++ rosmapExpression.R
@@ -0,0 +1,42 @@
+library(synapseClient)
+library(githubr)
+synapseLogin()
+
+exprDataObj <- synGet('syn4922930')
+geneIdObj <- synGet('syn4922926')
+sampleIdObj <- synGet('syn4922923')
+
+library(data.table)
+exprData <- fread(exprDataObj@filePath,data.table=F)
+geneId <- fread(geneIdObj@filePath,data.table=F,header = F)
+View(geneId)
+sampleId <- fread(sampleIdObj@filePath,data.table=F,header=F)
+View(sampleId)
+
+rownames(exprData) <- sampleId[,1]
+colnames(exprData) <- geneId[,1]
+
+write.csv(exprData,file='ROSMAP_Expression.csv',quote=F)
+
+#version comment
+comment = "ROSMAP expression data that was processed by Chris Gaiteri, with the gene names and sample ids added as row and column names"
+
+foo = File('ROSMAP_Expression.csv',parentId='syn7981630',versionComment = comment)
+
+bar2 = synGet('syn3505732',downloadFile=F)
+anno = synGetAnnotations(bar2)
+
+#annotations
+synSetAnnotations(foo) = as.list(anno)
+
+#provenance and store
+foo = synStore(foo,
+               used = as.list(c('syn4922930',
+                                'syn4922926',
+                                'syn4922923')),
+               executed = as.list())
+
+
+
+
+
