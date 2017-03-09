4b7d3f8bc8d856f4c9e5d2ebb5c6da8b31be46d8
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-03-09 15:37:36 -0800

    fixed method

--- networkComparison.R
+++ networkComparison.R
@@ -14,14 +14,17 @@ loadBic <- function(synId){
 ampNetworks <- lapply(bar$file.id,loadBic)
 names(ampNetworks) <- c('rosmap','mayoTcx','mayoCer','mssmFp','mssmStg','mssmPhg','mssmIfg')
 
-methodManifest <- expand.grid(names(ampNetworks),names(ampNetworks))
+#methodManifest <- expand.grid(names(ampNetworks),names(ampNetworks))
+#methodManifest <- dplyr::filter(methodManifest,Var1!=Var2)
+library(dplyr)
+methodManifest <- combn(names(ampNetworks),2) %>% t
 
 wrapperFxn <- function(method1,method2,allnets){
   return(metanetwork::compareTwoNetworks(allnets[[method1]],allnets[[method2]]))
 }
 
 networkCompare <- mapply(wrapperFxn,
-                         methodManifest[1:2,1],
-                         methodManifest[1:2,2],
+                         methodManifest[,1],
+                         methodManifest[,2],
                          MoreArgs = list(allnets=ampNetworks),
                          SIMPLIFY = FALSE)
