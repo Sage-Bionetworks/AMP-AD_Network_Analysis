9e96b0a8c84c7a0c849ec1b13db15b94531e1d4a
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-03-09 14:09:56 -0800

    updated comparison functions

--- networkComparison.R
+++ networkComparison.R
@@ -1,7 +1,27 @@
 #network comparison script
-library(synapseClient)
-synapseLogin()
-foo <- synQuery('select * from file where method==\'bic\' and projectId==\'syn2370594\'')
+synapseClient::synapseLogin()
+foo <- synapseClient::synQuery('select * from file where method==\'bic\' and projectId==\'syn2370594\'')
 
 bar <- dplyr::select(foo,file.name,file.id,file.versionComment)
 
+loadBic <- function(synId){
+  synapseClient::synapseLogin()
+  foo<-synapseClient::synGet(synId)
+  load(foo@filePath)
+  return(bicNetworks$network)
+}
+
+ampNetworks <- lapply(bar$file.id,loadBic)
+names(ampNetworks) <- c('rosmap','mayoTcx','mayoCer','mssmFp','mssmStg','mssmPhg','mssmIfg')
+
+methodManifest <- expand.grid(names(ampNetworks),names(ampNetworks))
+
+wrapperFxn <- function(method1,method2,allnets){
+  return(metanetwork::compareTwoNetworks(allnets[[method1]],allnets[[method2]]))
+}
+
+networkCompare <- mapply(wrapperFxn,
+                         methodManifest[1:2,1],
+                         methodManifest[1:2,2],
+                         MoreArgs = list(allnets=ampNetworks),
+                         SIMPLIFY = FALSE)
