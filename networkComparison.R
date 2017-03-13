0855d47434241a954fdd6b7fd74770b17324d46c
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-03-13 11:38:41 -0700

    updated network comparison script

--- networkComparison.R
+++ networkComparison.R
@@ -12,7 +12,15 @@ loadBic <- function(synId){
 }
 
 ampNetworks <- lapply(bar$file.id,loadBic)
-names(ampNetworks) <- c('rosmap','mayoTcx','mayoCer','mssmFp','mssmStg','mssmPhg','mssmIfg')
+names(ampNetworks) <- c('rosmap',
+                        'mayoTcx',
+                        'mayoCer',
+                        'mssmFp',
+                        'mssmStg',
+                        'mssmPhg',
+                        'mssmIfg',
+                        'mcadgsTCX',
+                        'mcadgsCER')
 
 #methodManifest <- expand.grid(names(ampNetworks),names(ampNetworks))
 #methodManifest <- dplyr::filter(methodManifest,Var1!=Var2)
@@ -28,3 +36,57 @@ networkCompare <- mapply(wrapperFxn,
                          methodManifest[,2],
                          MoreArgs = list(allnets=ampNetworks),
                          SIMPLIFY = FALSE)
+
+####extract odds ratios
+grabOddsRatios <- function(x){
+  return(x[[1]]$estimate)
+}
+
+ors <- sapply(networkCompare,grabOddsRatios)
+colnames(methodManifest) <- c('network1','network2')
+methodManifest <- data.frame(methodManifest,
+                             stringsAsFactors = F)
+methodManifest$ors <- ors
+
+
+
+
+distMat <- tidyr::spread(methodManifest,
+                         network1,
+                         ors)
+rownames(distMat) <- distMat$network2
+distMat <- dplyr::select(distMat,-network2)
+missingRow <- setdiff(colnames(distMat),rownames(distMat))
+missingCol <- setdiff(rownames(distMat),colnames(distMat))
+distMat[,missingCol] <- NA
+distMat[missingRow,] <- NA
+distMat <- distMat[,rownames(distMat)]
+distMat[(is.na(distMat))] <- 0
+distMat2 <- distMat + t(distMat)
+View(distMat2)
+svd2 <- svd(distMat2)
+library(wesanderson)
+
+makePcPlot <- function(svdecomp,i,j,nameVec){
+  rangex <- max(svd2$u[,i])-min(svd2$u[,i])
+  rangey <- max(svd2$u[,j])-min(svd2$u[,j])
+  xmin <- min(svd2$u[,i])-0.10*rangex
+  xmax <- max(svd2$u[,i])+0.10*rangex
+  ymin <- min(svd2$u[,j])-0.10*rangey
+  ymax <- max(svd2$u[,j])+0.10*rangey
+  plot(svd2$u[,i],
+       svd2$u[,j],
+       col='white',
+       xlim=c(xmin,xmax),
+       ylim=c(ymin,ymax),
+       xlab = paste0('PCA',i),
+       ylab = paste0('PCA',j))
+  text(svd2$u[,i],
+       svd2$u[,j],
+       nameVec,
+       col=wes_palette("Rushmore",7,type='continuous'))
+}
+makePcPlot(svd2,1,2,rownames(distMat))
+#par(mar=c(12,4,6,6))
+pheatmap::pheatmap(distMat2)
+
