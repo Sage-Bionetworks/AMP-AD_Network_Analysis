d1a070e34536f38d035f0de9a4d0c076c1b5144f
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-03-21 09:38:51 -0700

    updated test

--- moduleComparisonTest.R
+++ moduleComparisonTest.R
@@ -14,5 +14,12 @@ fxn1 <- function(x){
 baz2 <- lapply(baz,fxn1)
 
 ensembleOfCluster <- clue::cl_ensemble(list = baz2)
-cl_agreement(ensembleOfCluster,method = "euclidean")
-bar22 <- cl_consensus(ensembleOfCluster)
+clue::cl_agreement(ensembleOfCluster,method = "NMI")
+bar22 <- clue::cl_consensus(ensembleOfCluster)
+moduleAssignments <- apply(bar22$.Data,1,function(x){if(sum(x>.5)>0){return(which(x>.5))}else{return(NA)}})
+v1 <-utilityFunctions::convertEnsemblToHgnc(names(moduleAssignments))
+moduleAssignments2 <- data.frame(moduleLabels=moduleAssignments,
+                                 ensembl_gene_id=names(moduleAssignments),stringsAsFactors=F)
+v1 <- dplyr::left_join(v1,moduleAssignments2,"ensembl_gene_id")
+v_29 <- dplyr::filter(v1,moduleLabels==169)
+cat(v_29$external_gene_name,file='~/Desktop/mod29.csv',sep='\n')
