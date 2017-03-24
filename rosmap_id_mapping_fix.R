f2699b7436ec8d0cd28f81a57fe779c7675288d9
Ben Logsdon <ben.logsdon@sagebase.org>
2017-03-24 08:39:33 -0700

    updates

--- rosmap_id_mapping_fix.R
+++ rosmap_id_mapping_fix.R
@@ -62,13 +62,44 @@ expressionData$aSampleId <- rownames(expressionData)
 expressionData <- dplyr::left_join(expressionData,KEY,by=c('aSampleId'='Sampleid'))
 combinedData <- dplyr::left_join(expressionData,rosmapClinical,by='projid')
 
-lmObj <- lm(cogn_global_slope ~ age_death+ceradsc+braaksc+cogdx+apoe_genotype+ENSG00000123338,data = combinedData)
+lmObj <- lm(cogn_global_slope ~ age_death+ceradsc+braaksc+cogdx+apoe_genotype+ENSG00000182621,data = combinedData)
 summary(lmObj)
 load(synapseClient::getFileLocation(rosmapMetanetworkObj))
 
 rosmap <- igraph::graph_from_adjacency_matrix(bicNetworks$network,
                                               mode='undirected')
-degree <- igraph::authority_score(rosmap)$vector
+degree <- igraph::betweenness(rosmap)
 degree2 <- data.frame(ensembl_gene_id=names(degree),degree=degree,stringsAsFactors=F)
 degree2 <- dplyr::arrange(degree2,desc(degree))
 degree2[1:5,]
+
+foobar <- model.matrix(~1 + age_death+ceradsc+braaksc+cogdx+apoe_genotype,data=combinedData)
+a1 <-which(is.na(combinedData$cogn_global_slope))
+foobar <- foobar[-a1,]
+combinedData <- combinedData[-a1,]
+
+
+lmFun <- function(x,y,z){
+  lmObj <- lm(y~x+z-1)
+  return(summary(lmObj)$coef[1,4])
+}
+expressionHarmonized <- dplyr::select(combinedData,starts_with("ENSG"))
+expressionHarmonized <- expressionHarmonized[-a1,]
+foo2 <- apply(expressionHarmonized,2,lmFun,y=as.matrix(combinedData$cogn_global_slope),z=foobar)
+
+
+tabl23 <- data.frame(pval=foo2,ensembl_gene_id=names(foo2),stringsAsFactors = F)
+
+rosmapNetworkStats$ensembl_gene_id <- rownames(rosmapNetworkStats)
+
+tabl34 <- dplyr::left_join(rosmapNetworkStats,tabl23,by='ensembl_gene_id')
+geneMap <- utilityFunctions::convertEnsemblToHgnc(tabl34$ensembl_gene_id)
+tabl56 <- dplyr::left_join(tabl34,geneMap,by='ensembl_gene_id')
+View(tabl56)
+y <- -log10(tabl56$pval)
+summary(lm(y~degree+authority_score+closeness+eccentricity+eigen_centrality+centr_betw+transitivity,data=tabl56))
+betas <- summary(a11)$coef[,1]
+tabl56 <- dplyr::mutate(tabl56,networkFeature=betas[1] + betas[2]*degree + betas[3]*closeness + betas[4]*eccentricity + betas[5]*eigen_centrality + betas[6]*centr_betw)
+tabl56 <- dplyr::arrange(tabl56,desc(networkFeature))
+tabl56 <- dplyr::arrange(tabl56,(pval))
+cat(tabl56$external_gene_name[1:500],file='~/Desktop/networkz.csv',sep='\n')
