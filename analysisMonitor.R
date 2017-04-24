7a689cfc06f116a6b766262402791077794dd8de
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-04-24 13:00:01 -0700

    updated analysis monitor script

--- analysisMonitor.R
+++ analysisMonitor.R
@@ -9,4 +9,10 @@ View(bicNetworkManifest@values)
 consensusModuleManifest <- synapseClient::synTableQuery("SELECT * FROM syn8681664 WHERE ( ( study = 'MSBB' OR study = 'MayoRNAseq' OR study = 'ROSMAP' ) AND ( analysisType = 'consensusModuleIdentification' ) )")
 View(consensusModuleManifest@values)
 
+moduleManifest <- synapseClient::synTableQuery("SELECT * FROM syn8681664 WHERE ( ( study = 'MSBB' OR study = 'MayoRNAseq' OR study = 'ROSMAP' ) AND ( analysisType = 'moduleIdentification' ) AND ( method = 'CFnder' OR method = 'fast_greedy' OR method = 'GANXiS' OR method = 'infomap' OR method = 'label_prop' OR method = 'linkcommunities' OR method = 'linkcommunities' OR method = 'louvain' OR method = 'spinglass' OR method = 'walktrap' ) )")
+View(moduleManifest@values)
+table(moduleManifest@values$tissueOfOrigin)
+
+
 consortiaModules <- synapseClient::synTableQuery("SELECT * FROM syn8681664 WHERE ( ( study = 'MSBB' OR study = 'MayoRNAseq' OR study = 'ROSMAP' ) AND ( analysisType = 'consensusModuleIdentification' OR analysisType = 'moduleIdentification')  AND (method = 'kmeans' OR method = 'wina' OR method = 'speakeasy' OR method = 'megena' OR method = 'wgcna')) AND (columnScaled = 'TRUE')")
+View(consortiaModules@values)
