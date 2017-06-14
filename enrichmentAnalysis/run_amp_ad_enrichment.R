ad1db7a7f532fd1aeb7c92d84180c83e8f8a387a
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-06-14 12:29:36 -0700

    updated documentation

--- enrichmentAnalysis/run_amp_ad_enrichment.R
+++ enrichmentAnalysis/run_amp_ad_enrichment.R
@@ -9,6 +9,10 @@ run_amp_ad_enrichment <- function(geneSetList,
   #geneSetName - the names of the geneSets
   #hgnc - boolean indicating whether the gene identifiers are hgnc (TRUE) or ensembl (FALSE)
   #manifestId - location of a synapse table with the modules definitions in terms of hgnc names
+  
+  
+  #OUTPUT:
+  #data frame with module name, gene set name, p-value, and odds ratio of enrichment
   library(dplyr)
   cat('logging into Synapse...\n')
   synapseClient::synapseLogin()
