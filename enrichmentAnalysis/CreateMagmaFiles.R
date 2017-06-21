7035dbbf1acaa0e79fbf8c19adec904f824e6c1e
Sumit Mukherjee <mukhes3@uw.edu>
2017-06-21 14:02:01 -0700

    Adding documentation

--- enrichmentAnalysis/CreateMagmaFiles.R
+++ enrichmentAnalysis/CreateMagmaFiles.R
@@ -1,51 +1,72 @@
 Create.MAGMA.GeneLists <- function(ModuleList, OutputFileName,
                                    manifestId = "syn9770791"){
 
+  #The purpose of this function is to create a text file
+  #containing the genes in a set of modules
+
+  #Inputs:
+  #ModuleList: A list/vector containing the module names
+  #OutputFileName: The name of the output text file
+  #manifestId: manifestId in synapse
+
+  #Outputs:
+  #None
+
+  #pulling modules
   cat('pulling modules...\n')
 
 
   ModList <- list()
   for(i in 1:length(ModuleList))
   {
+    #loading genes in each module from synapse
     cat('Loading module ',i,' of ',length(ModuleList),'\n')
     temp <- synapseClient::synTableQuery(
       paste0("SELECT external_gene_name FROM ",manifestId," WHERE ModuleNameFull = ",
              "\'",ModuleList[i],"\'", sep= ""))@values
 
-    #cat('List size',length(temp$external_gene_name),'\n')
+
+    #appending genes in the extracted module to a list
     ModList <- append(ModList, as.list(ModuleList[i]))
     ModList <- as.list(append(ModList,
                               temp$external_gene_name))
     ModList <- append(ModList, as.list('\n'))
-    #typeof(ModList)
-    #Temp <- do.call(cbind, ModList)
-    #cat(Temp, sep = " ")
-    #cat("\n")
 
 
   }
 
-  #sink()
-  #writeLines(ModList,OutputFileName)
-  #sink(OutputFileName)
-  #print(ModList)
-  #sink()
-  #closeAllConnections()
+  #writing the genes by module into a file
   Temp <- paste(ModList, collapse = ' ')
   writeLines(Temp, OutputFileName)
-
-
 
   return(NULL)
 }
 
+
+
+
+
+
+
 GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
   manifestId = "syn9770791"){
 
+  #The purpose of this function is to get the minimum SNP p-value
+  #for all the genes in a module
+
+  #Inputs:
+  #ModName: Module name
+  #AnnotFile: File containing the gene annotations for the GWAS_file
+  #GWAS_file: File containing SNP p-values
+  #manifestId: manifestId for synapse
+
+  #Outputs:
+  #RetVar$GeneList: The list of genes in the module
+  #RetVar$Pval_min: The minimum p-value for each gene in the list
+
+
   #Getting genes in the module
   cat('Getting genes in module...\n')
-  #cat("SELECT external_gene_name FROM ",manifestId," WHERE ModuleNameFull = ",
-  #       "\'",ModName,"\'", sep= "")
   temp <- synapseClient::synTableQuery(
     paste0("SELECT external_gene_name FROM ",manifestId," WHERE ModuleNameFull = ",
            "\'",ModName[1],"\'", sep= ""))@values
@@ -54,8 +75,6 @@ GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
 
   #reading the list of genes from the annotation file
   cat('Reading the list of genes from the annotation file ...\n')
-  #Temp <- read.table(AnnotFile, skip = 2, header = FALSE, fill = TRUE)
-  #Temp <- Temp$V1
   con <- file(AnnotFile, open = 'r')
 
   Temp <- c()
@@ -79,6 +98,7 @@ GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
 
   Pval_list <- rep(1,length(GeneList))
 
+  #Finding the smallest p-value for each gene
   cat('Finding smallest p-value for each gene... \n')
 
   for ( i in 1:length(GeneList)){
@@ -86,7 +106,6 @@ GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
     cat('Gene',i,'of',length(GeneList),'...\n')
 
     In <- which(Temp %in% GeneList[i])
-    #print(In)
 
     if(length(In)==0){
       next
@@ -97,18 +116,11 @@ GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
 
     cat('Number of SNPs is',length(SNP_list),'\n')
 
+    #Converting the dataframe object to list
     SNP_list <- DF2List(SNP_list)
 
-    #cat('Converted to list \n')
-
-    #print(SNP_list)
-    #Pval_In <- unlist(lapply(SNP_list, function(x) grep(x, GWAS_snp)))
-    #print(typeof(Pval_In))
-    #Pval_temp <- Pval_list[Pval_In]
-    #print(Pval_temp)
-
+    #Getting the minimum p-value from a given set of SNPs
     Pval_In <- Get.Min.PvalIn(SNP_list, GWAS_snp)
-    #cat('Obtained index of minimum pval \n')
     Pval_temp <- GWAS_pval[Pval_In]
 
     Pval_list[i] <- min(Pval_temp)
@@ -124,8 +136,21 @@ GWAS.Enrich.Modules <- function(ModName, AnnotFile, GWAS_file,
 
 }
 
+
+
+
+
+
 DF2List <- function(DF){
+
   #function to convert one line dataframe to vector
+
+  #Inputs:
+  #DF: A one line dataframe
+
+  #Outputs:
+  #FinalList: A list containing the same data
+
   DF.list <- as.list(DF)
   my.vec <- lapply(DF.list, function(x){x[[1]][[1]]})
   my.final.vec <- as.data.frame(unlist(my.vec), stringsAsfactors = F)
@@ -133,20 +158,47 @@ DF2List <- function(DF){
   return(FinalList)
 }
 
+
+
+
+
+
+
 Get.Min.PvalIn <- function(SNP_list, GWAS_snp){
-  #Pval_In <- unlist(lapply(SNP_list, function(x) grep(x, GWAS_snp)))
-  #print(typeof(Pval_In))
-  #print(Pval_In)
+
+  #function to get the minimum p-value index for a given set of
+  #SNPs from a larger document containing all the SNPs and their
+  #p-values
+
+  #Inputs:
+  #SNP_list: A string list containing a subset of the total SNPs
+  #GWAS_snp: The list of total SNPs along with their p-values
+
+  #Outputs:
+  #Pval_In: The index of the smallest SNP
+
   Pval_In <- which(GWAS_snp %in% SNP_list)
 
   return(Pval_In)
-
 
 }
 
 
 
 Gen.GSEA.DF <- function(GSE_file, AnnotFile, GWAS_file){
+
+  #This file returns the the dataframe in the final form for synapse
+
+  #Inputs:
+  #GSE_file: List of modules with genes in each
+  #AnnotFile: GWAS annotation file
+  #GWAS_file: GWAS SNP and p-value file
+
+  #Output:
+  #GeneFrame: dataframe containing most enriched module and
+  #           pvalue of each gene in the module
+  #           also contains p-value of the module computer
+  #           by MAGMA 
 
   cat('Obtaining most enriched module in region .. \n')
   GWAS_enrich <- read.table(GSE_file, skip = 3, header = T)
