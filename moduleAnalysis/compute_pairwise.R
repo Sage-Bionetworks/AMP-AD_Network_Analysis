1b7aac544ccfe25d989a449f6e9bbf29121b04bf
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-08-16 14:41:45 -0700

    updated pairwise computation

--- moduleAnalysis/compute_pairwise.R
+++ moduleAnalysis/compute_pairwise.R
@@ -2,7 +2,7 @@ cat('logging into Synapse...\n')
 synapseClient::synapseLogin()
 #grab module definitions
 cat('pulling modules...\n')
-allMods <- synapseClient::synTableQuery(paste0("SELECT * FROM ","syn10163855"))@values
+allMods <- synapseClient::synTableQuery(paste0("SELECT * FROM ","syn10338156"))@values
 listify <- function(x,y,z){
   ###fxn will listify a long form table
   ###x: unique key
